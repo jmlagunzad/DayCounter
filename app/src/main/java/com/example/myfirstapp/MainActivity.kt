d9f658f52c7f9c39ae970a4495dc2e70e25a8b7d
jmlagunzad <may025jpl@gmail.com>
2020-09-07 10:36:00 +0800

    Changed typo in viewpager Endure to Educate

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -39,7 +39,7 @@ class MainActivity : AppCompatActivity() {
                     tab.text = "Explore"
                 }
                 else -> {
-                    tab.text = "Endure"
+                    tab.text = "Educate"
                 }
             }
         }.attach()
