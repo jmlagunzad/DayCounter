25092620ce1bfb7fe8eaf27b3d40ea96ac275055
jmlagunzad <may025jpl@gmail.com>
2020-10-05 14:26:46 +0800

    Clicking notifs will now open the app

--- app/src/main/java/com/example/myfirstapp/ReminderBroadcast.kt
+++ app/src/main/java/com/example/myfirstapp/ReminderBroadcast.kt
@@ -1,10 +1,13 @@
 package com.example.myfirstapp
 
+import android.app.Notification
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.graphics.BitmapFactory
+import android.media.RingtoneManager
 import android.os.Build
 import android.widget.Toast
 import androidx.annotation.RequiresApi
@@ -20,6 +23,11 @@ class ReminderBroadcast : BroadcastReceiver() {
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onReceive(context: Context, intent: Intent) {
+        val mainIntent = Intent(context, MainActivity::class.java).apply{
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        }
+        val pendingIntent = PendingIntent.getActivity(context,0,mainIntent, 0)
+
         val bitmap = BitmapFactory.decodeResource(context.applicationContext.resources, R.mipmap.joker_crit_foreground)
         val bitmapSmall = BitmapFactory.decodeResource(context.applicationContext.resources, R.mipmap.morgana_icon_foreground)
 
@@ -29,6 +37,7 @@ class ReminderBroadcast : BroadcastReceiver() {
             .setStyle(NotificationCompat.BigPictureStyle().bigPicture(bitmap))
             .setContentTitle("Looking cool, Joker!")
             .setContentText("You've been clean for ${computeDays(intent)} day/s!")
+            .setContentIntent(pendingIntent)
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
 
         val notificationManager: NotificationManagerCompat = NotificationManagerCompat.from(context)
