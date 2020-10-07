87044b84bb66ccfc595e4786be12221e2bcb7f04
jmlagunzad <may025jpl@gmail.com>
2020-10-07 16:36:11 +0800

    Added splash screen functionality, working commit

--- app/src/main/java/com/example/myfirstapp/SplashScreenActivity.kt
+++ app/src/main/java/com/example/myfirstapp/SplashScreenActivity.kt
@@ -1,11 +1,21 @@
 package com.example.myfirstapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_splash_screen.*
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        im_icon.alpha = 0f
+        im_icon.animate().setDuration(1500).alpha(1f).withEndAction{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
     }
 }
